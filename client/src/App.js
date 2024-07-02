8e0c86379c8a760283b0ae66e615d0a1b4fbbac2
amanpages <151524274+amanpages@users.noreply.github.com>
2024-07-03 01:54:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import Desktop from './pages/Desktop'
+import React from 'react';
+import Desktop from './pages/Desktop';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <Desktop/>
-  )
+    <>
+      <Desktop />
+      <ToastContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
